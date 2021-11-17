1c262da8e6bac921fb511f3e4b1f5258f87883fc
Gonzalo Puñales <lascano.gonzalo.ivan@gmail.com>
2021-11-16 23:26:04 -0300

    Previo a Instalacion Bootstrap

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,26 @@
 const NavBar = () => {
     return (
-        <div ClassName='navbody'>
-        <h1>
-            La Glorieta 
-        </h1>
-        <p>art and deco</p>
-        </div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">La Glorieta</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">Shop</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">Galería</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
     )
 }
 
