59ef01a4ac3159b43f9e8326e4c80ceacc759140
Gonzalo Puñales <lascano.gonzalo.ivan@gmail.com>
2021-11-16 23:12:29 -0300

    armando navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+const NavBar = () => {
+    return (
+        <div ClassName='navbody'>
+        <h1>
+            La Glorieta 
+        </h1>
+        <p>art and deco</p>
+        </div>
+    )
+}
+
+export default NavBar
