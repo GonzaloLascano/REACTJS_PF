0bebd78c52fa600024acbb523189d78b72e68847
Gonzalo Ivan Puniales Lascano <lascano.gonzalo.ivan@gmail.com>
2021-11-23 10:11:35 -0300

    shopping cart finished

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,8 +1,8 @@
 const CartWidget = () => {
     return(
-        <div className="CartContainer">
-            <i class="fi fi-rr-Shopping-cart"></i>
-            <p className="CartCount">0</p>
+        <div className="rounded p-2 bg-light shadow-sm CartContainer">
+            <img src="./assets/shopping-cart.png" alt="shopping cart" />
+            <p className="m-0 CartCount">0</p>
         </div>
     )
 }
