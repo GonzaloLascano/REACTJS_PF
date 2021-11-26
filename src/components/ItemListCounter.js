d1bac1ac76a48a9341f707aade07a9fe2a2a145b
Gonzalo Ivan Puniales Lascano <lascano.gonzalo.ivan@gmail.com>
2021-11-26 20:17:46 -0300

    se carga html del componente

--- src/components/ItemListCounter.js
+++ src/components/ItemListCounter.js
@@ -1,5 +1,17 @@
 const ItemListCounter = (props) => {
     return (
-        
+        <div className="counterContainer w-75 m-auto">
+        <div className="countInfo border bg-light mb-2 p-2">
+            <p className="itemDescription">
+                Faroles
+            </p>
+            <div className="counterHandles d-flex justify-content-between border p-2 rounded-pill">
+                <button type="button" className="btn btn-success rounded-pill">+</button>
+                <input type="text" className="border-0 shadow-none text-center" placeholder="cantidad productos">
+                <button type="button" className="btn btn-success rounded-pill">-</button>
+            </div>
+        </div>
+        <button type="button" className="btn btn-outline-success w-100">Agregar al Carrito</button>
+    </div>    
     );
 }
