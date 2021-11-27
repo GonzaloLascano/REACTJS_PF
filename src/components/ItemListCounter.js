5c00d99d05e2bc4e9c93ff2d36dc211fd7e26582
Gonzalo Ivan Puniales Lascano <lascano.gonzalo.ivan@gmail.com>
2021-11-26 21:48:18 -0300

    se termino de crear el componente itemListCounter

--- src/components/ItemListCounter.js
+++ src/components/ItemListCounter.js
@@ -1,17 +1,31 @@
+import { useState } from "react";
+
 const ItemListCounter = (props) => {
+    const [amount, setAmount] = useState(props.initial)
+
+    function addCount() {
+       amount < props.stock ? setAmount(amount + 1) : setAmount(amount + 0);
+    }
+
+    function substractCount() {
+        amount > 1? setAmount(amount - 1): setAmount(amount-0);
+    }
+    function onAdd() {
+        console.log("felicidades! agregaste" + amount + "producto/s a tu carrito");
+    }
     return (
         <div className="counterContainer w-75 m-auto">
         <div className="countInfo border bg-light mb-2 p-2">
             <p className="itemDescription">
-                Faroles
+                Faroles: {props.stock} en stock.
             </p>
             <div className="counterHandles d-flex justify-content-between border p-2 rounded-pill">
-                <button type="button" className="btn btn-success rounded-pill">+</button>
-                <input type="text" className="border-0 shadow-none text-center" placeholder="cantidad productos">
-                <button type="button" className="btn btn-success rounded-pill">-</button>
+                <button type="button" className="btn btn-success rounded-pill" onClick={addCount}>+</button>
+                <p class="border-bottom w-25 m-0 align-middle text-center">{amount}</p>
+                <button type="button" className="btn btn-success rounded-pill" onClick={substractCount}>-</button>
             </div>
         </div>
-        <button type="button" className="btn btn-outline-success w-100">Agregar al Carrito</button>
+        <button type="button" className="btn btn-outline-success w-100" onClick={onAdd}>Agregar al Carrito</button>
     </div>    
     );
 }
